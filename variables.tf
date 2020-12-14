0a8612a8c7eb06c01df69046dcd0e287f84d7807
Jimmy <Jimmy0319@gmail.com>
2020-12-14 11:38:24 +0800

    fix bot value

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "jx_bot_token" {
   description = "Bot token used to interact with the Jenkins X cluster git repository"
   type        = string
+  default     = "56f513d0f9d94fb669905421e3bd03c85de565cb"
 }
