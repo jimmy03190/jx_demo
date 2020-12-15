ea6f627193573733cbb41bab81f0e3d84878aaf9
Jimmy <Jimmy0319@gmail.com>
2020-12-15 15:41:57 +0800

    Change back token value

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "jx_bot_token" {
   description = "Bot token used to interact with the Jenkins X cluster git repository"
   type        = string
-  default     = "ddd5958103284a2a8993aa92348a1f805974a20f"
+  default     = "56f513d0f9d94fb669905421e3bd03c85de565cb"
 }
